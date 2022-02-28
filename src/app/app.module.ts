7898eb92a3cb6440ec34a29ba086032076872150
MaxAake <maxgustafsson.ugglum@gmail.com>
2022-02-28 10:44:09 +0100

    5 basic components set up
    Set up components: Selector, Container, Weather-alerts, Weather-forecast, Weather-current.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectorComponent } from './selector/selector.component';
+import { ContainerComponent } from './container/container.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+import { WeatherCurrentComponent } from './weather-current/weather-current.component';
+import { WeatherAlertsComponent } from './weather-alerts/weather-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectorComponent,
+    ContainerComponent,
+    WeatherForecastComponent,
+    WeatherCurrentComponent,
+    WeatherAlertsComponent
   ],
   imports: [
     BrowserModule,
