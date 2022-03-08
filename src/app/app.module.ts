259229f30f2472b29223270785ebe4289e79f77d
Harry norrman <harry.norrman@gmail.com>
2022-03-08 11:45:16 +0100

    weatherCurr updated
    Calls api correctly observable used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectorComponent } from './selector/selector.component';
@@ -10,6 +10,7 @@ import { WeatherCurrentComponent } from './weather-current/weather-current.compo
 import { WeatherAlertsComponent } from './weather-alerts/weather-alerts.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { Component } from '@angular/core';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
