4d82d50a3ab0fd93d87631ae6f85ca341649cce3
WilliamEmami <wi1653em-s@student.lu.se>
2022-03-09 23:45:05 +0100

    Update environment.prod.ts
    Just a thing thats needed... "ng build" uses this file and we dont have firebase here so i just copy pasted it from environment.ts and it works now

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC1gjPYJUjnTkMk4I9UdIgMHBrvWS5c5Hg',
+    authDomain: 'cities-f63da.firebaseapp.com',
+    projectId: 'cities-f63da',
+    storageBucket: 'cities-f63da.appspot.com',
+    messagingSenderId: '174658720056',
+    appId: '1:174658720056:web:bc27886c30af3e383631cd',
+    measurementId: 'G-1LC6TCZB9Z',
+  },
 };
