258647716ce47545f7573e6051f5a22d9e324470
WilliamEmami <wi1653em-s@student.lu.se>
2022-03-10 19:55:25 +0100

    Alerts (not functioning)
    They are not working as of yet but we're trying to fix it.

--- src/app/coordinates.service.ts
+++ src/app/coordinates.service.ts
@@ -3,38 +3,51 @@ import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoordinatesService {
-  lat: number
-  lon: number
-  current: Subject<any>
-  forecast: Subject<any>
+  lat: number;
+  lon: number;
+  current: Subject<any>;
+  forecast: Subject<any>;
+  alert: Subject<any>;
   constructor(private http: HttpClient) {
-    this.lat = 60;
-    this.lon = 60;
+    this.lat = 44.9;
+    this.lon = 14.6;
     this.current = new Subject();
     this.forecast = new Subject();
+    this.alert = new Subject();
   }
-  getCurrentWeather():void{
+  getCurrentWeather(): void {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&units=metric&appid=e013ee4b357a1f6290404c173646e3ce`;
     this.http.get(url).subscribe({
-      next: result => this.current.next(result)
-    })
+      next: (result) => this.current.next(result),
+    });
   }
 
-  getWeatherForecast():void{
+  getWeatherForecast(): void {
     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lon}&exclude=current,minutely,daily,alerts&units=metric&appid=e013ee4b357a1f6290404c173646e3ce`;
     this.http.get(url).subscribe({
-      next: result => this.forecast.next(result)
-    })
+      next: (result) => this.forecast.next(result),
+    });
   }
 
-  getCurrentResults():Subject<Object>{
+  getAlert(): void {
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lon}&exclude=current,minutely,hourly,daily&units=metric&appid=e013ee4b357a1f6290404c173646e3ce`;
+    this.http.get(url).subscribe({
+      next: (result) => this.alert.next(result),
+    });
+  }
+
+  getCurrentResults(): Subject<Object> {
     return this.current;
   }
 
-  getForecastResults():Subject<Object>{
+  getForecastResults(): Subject<Object> {
     return this.forecast;
+  }
+
+  getAlertResults(): Subject<Object> {
+    return this.alert;
   }
 }
--- src/app/weather-alerts/weather-alerts.component.ts
+++ src/app/weather-alerts/weather-alerts.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, NgModule } from '@angular/core';
+import { of } from 'rxjs';
+import { switchMap, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CoordinatesService } from '../coordinates.service';
 
 @Component({
   selector: 'app-weather-alerts',
   templateUrl: './weather-alerts.component.html',
-  styleUrls: ['./weather-alerts.component.css']
+  styleUrls: ['./weather-alerts.component.css'],
 })
+@Injectable()
 export class WeatherAlertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  alertInfo: { main: []; sender_name: ''; start: 0; end: 0 };
+  main: Observable<any[]>;
+  sender_name: '';
+  start: number;
+  end: number;
+  coordinates: CoordinatesService;
+  constructor(private http: HttpClient, coordinates: CoordinatesService) {
+    this.alertInfo = { main: [], sender_name: '', start: 0, end: 0 };
+    this.main = new Observable<any[]>();
+    this.sender_name = '';
+    this.start = 0;
+    this.end = 0;
+    this.coordinates = coordinates;
   }
 
+  ngOnInit(): void {
+    this.getAlertData();
+  }
+
+  getAlertData(): void {
+    this.coordinates.getAlertResults().subscribe((data) => {
+      this.parseData(data);
+    });
+  }
+
+  parseData(data: any) {
+    this.alertInfo = data;
+    console.log('halååååå');
+    this.main = of(Object.entries(this.alertInfo.main));
+    this.sender_name = this.alertInfo['sender_name'];
+    this.start = this.alertInfo['start'];
+    this.end = this.alertInfo['end'];
+  }
+  Date(time: number) {
+    let date = new Date(time.valueOf());
+    let day = date.toLocaleDateString('sv-SE');
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    return `${day} ${hour}:${minutes}0`;
+  }
+  currentAlertValues(index: number) {
+    const values = {
+      parameters: ['Alert from', 'Start time', 'End time', 'Alert'],
+    };
+    return values.parameters[index];
+  }
 }
